28ca83cc57b2a3f3bc089ee7e22ba3d9671752c8
micklop83 <micklop83@gmail.com>
2026-09-24 11:18:08 +0000

    miquel static family API

--- src/app.py
+++ src/app.py
@@ -30,12 +30,37 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+
+    return jsonify(member), 200
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    member = request.json
+    new_member = jackson_family.add_member(member)
+    return jsonify(new_member), 200
+
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.get_member(member_id)
+
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+
+    jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
 
 
 
